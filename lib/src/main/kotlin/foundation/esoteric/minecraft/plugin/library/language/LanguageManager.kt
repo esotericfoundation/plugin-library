3930140a13a96def6d801ae1602fdd226e94e72e
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-22 12:26:02 +0100

    Create beginning of LanguageManager file

---
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugin/library/language/LanguageManager.kt
@@ -0,0 +1,9 @@
+package foundation.esoteric.minecraft.plugin.library.language
+
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+
+class LanguageManager(plugin: JavaPlugin) {
+
+    val config: FileConfiguration = plugin.config
+}
