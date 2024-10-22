a1b203eaa9324bc85f68a57e2eb4e65e99d5a6da
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-22 12:35:36 +0100

    Add some documentation

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugin/library/language/LanguageManager.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugin/library/language/LanguageManager.kt
@@ -3,6 +3,14 @@ package foundation.esoteric.minecraft.plugin.library.language
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * The language manager class provides utility for dealing with
+ * internationalising your plugin, which means allowing messages
+ * to be in a language that the player has selected.
+ *
+ * If you intend to use the language manager, create a "languages"
+ * folder in your "resources" directory.
+ */
 class LanguageManager(plugin: JavaPlugin) {
 
     val config: FileConfiguration = plugin.config
