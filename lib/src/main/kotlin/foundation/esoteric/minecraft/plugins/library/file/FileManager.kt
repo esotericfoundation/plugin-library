aed6454780cd227220ac7c12ebe265f3ae52354c
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-28 22:41:21 +0000

    Return new folder after saving resource folder

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManager.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManager.kt
@@ -2,17 +2,20 @@ package foundation.esoteric.minecraft.plugins.library.file
 
 import foundation.esoteric.utility.resource.ResourceUtility
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 import java.nio.file.Path
 
 class FileManager(private val plugin: JavaPlugin) {
 
-    fun saveResourceFolder(resourcePath: Path, replace: Boolean = false) {
+    fun saveResourceFolder(resourcePath: Path, replace: Boolean = false): File {
         ResourceUtility.getResourceFilePaths(resourcePath).forEach {
             path -> plugin.saveResource(path.toString(), replace)
         }
+
+        return File(plugin.dataFolder, resourcePath.toString())
     }
 
-    fun saveResourceFolder(resourcePath: String, replace: Boolean = false) {
-        saveResourceFolder(Path.of(resourcePath), replace)
+    fun saveResourceFolder(resourcePath: String, replace: Boolean = false): File {
+        return saveResourceFolder(Path.of(resourcePath), replace)
     }
 }
