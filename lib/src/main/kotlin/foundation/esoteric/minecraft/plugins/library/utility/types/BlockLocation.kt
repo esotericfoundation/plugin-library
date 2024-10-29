86e5c3cdbf18e3779f10117a2331793952bab11d
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-29 22:46:16 +0000

    Add some documentation

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/utility/types/BlockLocation.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/utility/types/BlockLocation.kt
@@ -3,7 +3,18 @@ package foundation.esoteric.minecraft.plugins.library.utility.types
 import org.bukkit.Location
 import org.bukkit.block.Block
 
+/**
+ * A utility class that represents the location of one block.
+ * @param worldName The name of the world that this block is located in.
+ * @param x The x location of this block.
+ * @param y The y location of this block.
+ * @param z The z location of this block.
+ */
 class BlockLocation(val worldName: String, val x: Int, val y: Int, val z: Int) {
+    /**
+     * Gets the location of the block at this Location.
+     * @param location The location to get the block location of.
+     */
     constructor(location: Location) : this(
         location.world.name,
         location.x.toInt(),
@@ -11,5 +22,9 @@ class BlockLocation(val worldName: String, val x: Int, val y: Int, val z: Int) {
         location.z.toInt()
     )
 
+    /**
+     * Gets the location of this block.
+     * @param block The block to get the BlockLocation of.
+     */
     constructor(block: Block) : this(block.location)
 }
