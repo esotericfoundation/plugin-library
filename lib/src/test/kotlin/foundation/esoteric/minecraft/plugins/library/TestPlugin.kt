8b22a44114da5479aeb797b2b44fbbca03c12879
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-29 17:32:29 +0000

    Modularise plugin tests

--- lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/TestPlugin.kt
+++ lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/TestPlugin.kt
@@ -1,13 +1,5 @@
 package foundation.esoteric.minecraft.plugins.library
 
-import foundation.esoteric.minecraft.plugins.library.file.FileManager
 import org.bukkit.plugin.java.JavaPlugin
 
-open class TestPlugin : JavaPlugin() {
-
-    lateinit var fileManager: FileManager
-
-    override fun onEnable() {
-        fileManager = FileManager(this)
-    }
-}
+open class TestPlugin : JavaPlugin()
--- lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
@@ -9,9 +9,11 @@ import kotlin.test.assertTrue
 class FileManagerTest {
     @Test fun savingResourceWorksCorrectly() {
         MockBukkit.mock()
-        val plugin = MockBukkit.load(TestPlugin::class.java)
 
-        plugin.fileManager.saveResourceFolder("file/FileManagerTest")
+        val plugin = MockBukkit.load(TestPlugin::class.java)
+        val fileManager = FileManager(plugin)
+
+        fileManager.saveResourceFolder("file/FileManagerTest")
 
         assertTrue(File(plugin.dataFolder, "file").exists())
         assertTrue(File(plugin.dataFolder, "file/FileManagerTest").exists())
