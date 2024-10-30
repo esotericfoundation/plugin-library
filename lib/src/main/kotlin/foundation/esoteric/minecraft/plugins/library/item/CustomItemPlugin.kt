1521565068005c98f76a22b3c62f4002001137b3
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-30 22:17:33 +0000

    Add documentation to CustomItemPlugin

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/CustomItemPlugin.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/CustomItemPlugin.kt
@@ -2,6 +2,16 @@ package foundation.esoteric.minecraft.plugins.library.item
 
 import org.bukkit.plugin.Plugin
 
+/**
+ * Represents a plugin that implements custom items.
+ *
+ * You must implement this interface if you wish to use custom items in your plugin.
+ */
 interface CustomItemPlugin : Plugin {
+    /**
+     * The custom item manager associated with this plugin.
+     *
+     * A plugin must implement CustomItemPlugin to use the custom item manager.
+     */
     val customItemManager: CustomItemManager
 }
