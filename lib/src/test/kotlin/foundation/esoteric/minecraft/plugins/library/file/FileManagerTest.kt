31b57ab415826a5490a56de8e2d8a7cee6e4cdcd
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-30 23:08:48 +0000

    Update tests

--- lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
@@ -8,25 +8,22 @@ import kotlin.test.*
 class FileManagerTest {
 
     private lateinit var plugin: TestPlugin
-    private lateinit var fileManager: FileManager
 
     @BeforeTest fun mockFileManager() {
         MockBukkit.mock()
         plugin = MockBukkit.load(TestPlugin::class.java)
-        fileManager = FileManager(plugin)
     }
 
     @Test fun savingFolderWorks() {
-        fileManager.saveResourceFolder("file/FileManagerTest/1")
+        plugin.fileManager.saveResourceFolder("file/FileManagerTest")
 
         val fileDir = File(plugin.dataFolder, "file")
         val fileManagerTestDir = File(fileDir, "FileManagerTest")
-        val testOneDir = File(fileManagerTestDir, "1")
 
-        val testFileOne = File(testOneDir, "Test File 1.txt")
-        val testFileTwo = File(testOneDir, "Test File 2.txt")
+        val testFileOne = File(fileManagerTestDir, "Test File 1.txt")
+        val testFileTwo = File(fileManagerTestDir, "Test File 2.txt")
 
-        val subFolder = File(testOneDir, "Subfolder")
+        val subFolder = File(fileManagerTestDir, "Subfolder")
 
         val testFileThree = File(subFolder, "Test File 3.txt")
 
@@ -38,12 +35,7 @@ class FileManagerTest {
         assertTrue(fileManagerTestDir.exists())
         assertTrue(fileManagerTestDir.isDirectory)
         assertNotNull(fileManagerTestDir.listFiles())
-        assertEquals(fileManagerTestDir.listFiles()!!.size, 1)
-
-        assertTrue(testOneDir.exists())
-        assertTrue(testOneDir.isDirectory)
-        assertNotNull(testOneDir.listFiles())
-        assertEquals(testOneDir.listFiles()!!.size, 3)
+        assertEquals(fileManagerTestDir.listFiles()!!.size, 3)
 
         assertTrue(testFileOne.exists())
         assertFalse(testFileOne.isDirectory)
