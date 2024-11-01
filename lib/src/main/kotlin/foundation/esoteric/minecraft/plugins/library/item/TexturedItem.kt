4657b700858752c466b2e2160a1fcdf2c90ae98f
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-01 17:30:44 +0000

    Don't use resource pack
    A texture pack will be used instead.

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/TexturedItem.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/TexturedItem.kt
@@ -2,21 +2,18 @@ package foundation.esoteric.minecraft.plugins.library.item
 
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
-import java.nio.file.Path
 
-class TexturedItem : CustomItem {
+class TexturedItem(plugin: CustomItemPlugin, itemId: String, material: Material) : CustomItem(plugin, itemId, material) {
 
     companion object {
         var startingModelData: Int? = null
         var texturedItems: Int = 0
     }
 
-    constructor(plugin: CustomItemPlugin, itemId: String, material: Material, resourcePath: Path) : super(plugin, itemId, material) {
+    init {
         startingModelData = plugin.name.hashCode()
         texturedItems++
     }
-
-    constructor(plugin: CustomItemPlugin, itemId: String, material: Material, resourcePath: String) : this(plugin, itemId, material, Path.of(resourcePath))
 
     override fun toItem(item: ItemStack): ItemStack {
         super.toItem(item)
