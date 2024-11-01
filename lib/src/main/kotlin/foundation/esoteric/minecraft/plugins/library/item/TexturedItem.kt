ce2336abea90d61e4670ee4b764636f95adb6757
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-01 17:33:54 +0000

    Use the CLASS name hashCode
    This guarantees that there will be no collisions.

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/TexturedItem.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/item/TexturedItem.kt
@@ -4,23 +4,12 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 class TexturedItem(plugin: CustomItemPlugin, itemId: String, material: Material) : CustomItem(plugin, itemId, material) {
-
-    companion object {
-        var startingModelData: Int? = null
-        var texturedItems: Int = 0
-    }
-
-    init {
-        startingModelData = plugin.name.hashCode()
-        texturedItems++
-    }
-
     override fun toItem(item: ItemStack): ItemStack {
         super.toItem(item)
 
         item.editMeta {
             meta ->
-            meta.setCustomModelData(startingModelData!! + texturedItems)
+            meta.setCustomModelData(this::class.qualifiedName.hashCode())
         }
 
         return item
