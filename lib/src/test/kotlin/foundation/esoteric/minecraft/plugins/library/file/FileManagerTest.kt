117c15e6c3f8ba36a9aad0ba601439bb29644bcd
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-01 22:30:05 +0000

    Make file manager tests more modular

--- lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
+++ lib/src/test/kotlin/foundation/esoteric/minecraft/plugins/library/file/FileManagerTest.kt
@@ -9,53 +9,65 @@ class FileManagerTest {
 
     private var plugin: TestPlugin? = null
 
+    private var fileDir: File? = null
+    private var fileManagerTestDir: File? = null
+
+    private var testFileOne: File? = null
+    private var testFileTwo: File? = null
+    private var resourcePack: File? = null
+
+    private var subFolder: File? = null
+
+    private var testFileThree: File? = null
+
     @BeforeTest fun mockFileManager() {
         MockBukkit.mock()
         plugin = MockBukkit.load(TestPlugin::class.java)
         plugin!!.fileManager.saveResourceFolder("file/FileManagerTest")
+
+        fileDir = File(plugin!!.dataFolder, "file")
+        fileManagerTestDir = File(fileDir, "FileManagerTest")
+
+        testFileOne = File(fileManagerTestDir, "Test File 1.txt")
+        testFileTwo = File(fileManagerTestDir, "Test File 2.txt")
+        resourcePack = File(fileManagerTestDir, "TestPluginResourcePack.zip")
+
+        subFolder = File(fileManagerTestDir, "Subfolder")
+
+        testFileThree = File(subFolder, "Test File 3.txt")
     }
 
     @Test fun savingFolderWorks() {
-        val fileDir = File(plugin!!.dataFolder, "file")
-        val fileManagerTestDir = File(fileDir, "FileManagerTest")
 
-        val testFileOne = File(fileManagerTestDir, "Test File 1.txt")
-        val testFileTwo = File(fileManagerTestDir, "Test File 2.txt")
-        val resourcePack = File(fileManagerTestDir, "TestPluginResourcePack.zip")
+        assertTrue(fileDir!!.exists())
+        assertTrue(fileDir!!.isDirectory)
+        assertNotNull(fileDir!!.listFiles())
+        assertEquals(fileDir!!.listFiles()!!.size, 1)
 
-        val subFolder = File(fileManagerTestDir, "Subfolder")
+        assertTrue(fileManagerTestDir!!.exists())
+        assertTrue(fileManagerTestDir!!.isDirectory)
+        assertNotNull(fileManagerTestDir!!.listFiles())
+        assertEquals(fileManagerTestDir!!.listFiles()!!.size, 4)
 
-        val testFileThree = File(subFolder, "Test File 3.txt")
+        assertTrue(testFileOne!!.exists())
+        assertFalse(testFileOne!!.isDirectory)
+        assertEquals(testFileOne!!.readText().trimEnd('\r', '\n'), "This file is used to test the FileManager.")
 
-        assertTrue(fileDir.exists())
-        assertTrue(fileDir.isDirectory)
-        assertNotNull(fileDir.listFiles())
-        assertEquals(fileDir.listFiles()!!.size, 1)
+        assertTrue(testFileTwo!!.exists())
+        assertFalse(testFileTwo!!.isDirectory)
+        assertEquals(testFileTwo!!.readText().trimEnd('\r', '\n'), "This file is used to test the FileManager.")
 
-        assertTrue(fileManagerTestDir.exists())
-        assertTrue(fileManagerTestDir.isDirectory)
-        assertNotNull(fileManagerTestDir.listFiles())
-        assertEquals(fileManagerTestDir.listFiles()!!.size, 4)
+        assertTrue(resourcePack!!.exists())
+        assertFalse(resourcePack!!.isDirectory)
 
-        assertTrue(testFileOne.exists())
-        assertFalse(testFileOne.isDirectory)
-        assertEquals(testFileOne.readText().trimEnd('\r', '\n'), "This file is used to test the FileManager.")
+        assertTrue(subFolder!!.exists())
+        assertTrue(subFolder!!.isDirectory)
+        assertNotNull(subFolder!!.listFiles())
+        assertEquals(subFolder!!.listFiles()!!.size, 1)
 
-        assertTrue(testFileTwo.exists())
-        assertFalse(testFileTwo.isDirectory)
-        assertEquals(testFileTwo.readText().trimEnd('\r', '\n'), "This file is used to test the FileManager.")
-
-        assertTrue(resourcePack.exists())
-        assertFalse(resourcePack.isDirectory)
-
-        assertTrue(subFolder.exists())
-        assertTrue(subFolder.isDirectory)
-        assertNotNull(subFolder.listFiles())
-        assertEquals(subFolder.listFiles()!!.size, 1)
-
-        assertTrue(testFileThree.exists())
-        assertFalse(testFileThree.isDirectory)
-        assertEquals(testFileThree.readText().trimEnd('\r', '\n'), "A third test file for the FileManagerTest.")
+        assertTrue(testFileThree!!.exists())
+        assertFalse(testFileThree!!.isDirectory)
+        assertEquals(testFileThree!!.readText().trimEnd('\r', '\n'), "A third test file for the FileManagerTest.")
     }
 
     @Test fun resourcePackHashWorks() {
