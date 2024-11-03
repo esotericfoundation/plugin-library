f22007326b6c5f5a0b7229f4900837d73e98c0e0
Esoteric Enderman <esotericenderman@gmail.com>
2024-11-03 13:30:17 +0000

    Update block location docs

--- lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/utility/types/BlockLocation.kt
+++ lib/src/main/kotlin/foundation/esoteric/minecraft/plugins/library/utility/types/BlockLocation.kt
@@ -5,15 +5,21 @@ import org.bukkit.block.Block
 
 /**
  * A utility class that represents the location of one block.
- * @param worldName The name of the world that this block is located in.
+ * @param worldName The name of the `World` that this block is located in.
  * @param x The x location of this block.
  * @param y The y location of this block.
  * @param z The z location of this block.
+ * @property worldName The name of the `World` that this block is located in.
+ * @property x The x location of this block.
+ * @property y The y location of this block.
+ * @property z The z location of this block.
+ * @author Esoteric Enderman
  */
 class BlockLocation(val worldName: String, val x: Int, val y: Int, val z: Int) {
     /**
-     * Gets the location of the block at this Location.
-     * @param location The location to get the block location of.
+     * Gets the location of the block at this `Location`.
+     * @param location The `Location` to get the `BlockLocation` of.
+     * @author Esoteric Enderman
      */
     constructor(location: Location) : this(
         location.world.name,
@@ -23,8 +29,9 @@ class BlockLocation(val worldName: String, val x: Int, val y: Int, val z: Int) {
     )
 
     /**
-     * Gets the location of this block.
-     * @param block The block to get the BlockLocation of.
+     * Gets the location of this `Block`.
+     * @param block The `Block` to get the `BlockLocation` of.
+     * @author Esoteric Enderman
      */
     constructor(block: Block) : this(block.location)
 }
